17df671173640c0bf69b6defff561dd9ba69b03e
Stefan Freitag <stefan.freitag@udo.edu>
2019-06-09 21:11:11 +0200

    Created serverless admin

--- lib/serverless-course-stack.ts
+++ lib/serverless-course-stack.ts
@@ -1,9 +1,22 @@
-import cdk = require('@aws-cdk/cdk');
+import cdk = require("@aws-cdk/cdk");
 
+import { User, Policy, CfnAccessKey } from "@aws-cdk/aws-iam";
+import { CfnOutput } from "@aws-cdk/cdk";
 export class ServerlessCourseStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const user = new User(this, "serverless-admin-user", {
+      userName: "serverless-admin"
+    });
+    user.attachManagedPolicy("arn:aws:iam::aws:policy/AdministratorAccess");
+    const accessKey = new CfnAccessKey(this, "myAccessKey", {
+      userName: user.userName
+    });
+
+    new CfnOutput(this, "accessKeyId", { value: accessKey.accessKeyId });
+    new CfnOutput(this, "secretAccessKey", {
+      value: accessKey.accessKeySecretAccessKey
+    });
   }
 }
