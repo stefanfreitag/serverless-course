c235e1b887a15cea3ea409e8051a210b558b8dc4
Stefan Freitag <stefan.freitag@udo.edu>
2020-03-26 21:57:44 +0100

    chore: Moved to cdk v1.31.0

--- lib/serverless-course-stack.ts
+++ lib/serverless-course-stack.ts
@@ -1,42 +1,52 @@
-import cdk = require("@aws-cdk/cdk");
 
-import { User, Policy, CfnAccessKey, Role, ServicePrincipal } from "@aws-cdk/aws-iam";
-import { Bucket, EventType } from '@aws-cdk/aws-s3';
-import { CfnOutput } from "@aws-cdk/cdk";
-export class ServerlessCourseStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+import { User, CfnAccessKey, Role, ServicePrincipal, ManagedPolicy } from "@aws-cdk/aws-iam";
+import { Bucket } from "@aws-cdk/aws-s3";
+import { Table, Attribute, AttributeType, ProjectionType } from "@aws-cdk/aws-dynamodb";
+import { CfnOutput, PhysicalName, Stack, Construct, StackProps } from "@aws-cdk/core";
+export class ServerlessCourseStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
     const user = new User(this, "serverless-admin-user", {
       userName: "serverless-admin"
     });
-    user.attachManagedPolicy("arn:aws:iam::aws:policy/AdministratorAccess");
+    
+    user.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName("AdministratorAccess"));
     const accessKey = new CfnAccessKey(this, "myAccessKey", {
       userName: user.userName
     });
 
-    new CfnOutput(this, "accessKeyId", { value: accessKey.accessKeyId });
+    new CfnOutput(this, "accessKeyId", { value: accessKey.userName});
     new CfnOutput(this, "secretAccessKey", {
-      value: accessKey.accessKeySecretAccessKey
+      value: accessKey.attrSecretAccessKey
     });
 
-
-    const role = new Role(this, 'ImageResizingLambdaRole', {
-      assumedBy: new ServicePrincipal('lambda.amazonaws.com'),
-      roleName: 'lambda_s3_execution',
+    const role = new Role(this, "ImageResizingLambdaRole", {
+      assumedBy: new ServicePrincipal("lambda.amazonaws.com"),
+      roleName: "lambda_s3_execution"
     });
-    role.attachManagedPolicy('arn:aws:iam::aws:policy/AmazonS3FullAccess');
+    role.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName("AmazonS3FullAccess"));
 
-    const srcBucket = new Bucket(this,'ImageResizeSourceBucket', {
-        bucketName: 'de.freitag.stefan.imageresize'
-    });
+    const srcBucket = new Bucket(this, "ImageResizeSourceBucket", {
+      bucketName: "de.freitag.stefan.imageresize"
+        });
     // TODO: srcBucket.addEventNotification(EventType.ObjectCreatedPut, )
 
-    const destinationBucket = new Bucket(this,'ImageResizeDestinationBucket', {
-      bucketName: 'de.freitag.stefan.imageresize-dest'
-  });
+    const destinationBucket = new Bucket(this, "ImageResizeDestinationBucket", {
+      bucketName: "de.freitag.stefan.imageresize-dest"
+    });
 
+    const table = new Table(this, "Table", {
+      tableName: "td_notes",
+      partitionKey: { name: "user_id", type: AttributeType.STRING },
+      sortKey: { name: "timestamp", type: AttributeType.NUMBER },
+      
+    });
+   /**  table.addLocalSecondaryIndex({
+      indexName: "user_id-title-index",
+      
+      projectionType: ProjectionType.All
+    })
+    */
   }
-
-
 }
